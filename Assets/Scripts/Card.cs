c9dcd1aa536d73c228ded9c42a295cea401f7ac3
Jason Tu <jasont@remarkablefoods.com>
2022-07-27 08:59:39 -0400

    Initial ScriptableObject for CardStatistics

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+/// <summary>
+/// TODO: Display this card.
+/// </summary>
+public class CardStatistics : ScriptableObject
+{
+    [SerializeField]
+    [Tooltip("Also known as ATK. Displayed on the lower-left of the card.")]
+    int attack;
+
+    [SerializeField]
+    [Tooltip("Also known as HP. Displayed on the lower-right of the card.")]
+    int health;
+
+    /// <summary>
+    /// The current health of the card when in play.
+    /// </summary>
+    int currentHealth;
+
+    /// <summary>
+    /// The upper-right portion of the card. Indicates the number of sacrifices needed to play the card.
+    /// </summary>
+    int summonCost;
+
+    // POST-MVP TODO:
+    //
+    // Bones – Conversely, there are cards that cost bones. You earn bones
+    // whenever your cards get destroyed by an opponent. This mana/resource
+    // will be introduced as a mechanic after your first loss or “death”
+    // against Leshy.
+
+    void OnEnable()
+    {
+        currentHealth = health;
+    }
+}
+
+public class Card : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

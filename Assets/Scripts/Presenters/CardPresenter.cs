f86e32b4358d5c4333e4f171864d8ed6cfa769a6
Jason Tu <jasont@remarkablefoods.com>
2022-08-09 08:49:17 -0400

    Add glow feedback

--- Assets/Scripts/Presenters/CardPresenter.cs
+++ Assets/Scripts/Presenters/CardPresenter.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 
-public class CardPresenter : MonoBehaviour
+public class CardPresenter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [field: SerializeField]
     public Card Card
@@ -24,11 +25,26 @@ public class CardPresenter : MonoBehaviour
     [SerializeField]
     TMP_Text nameText;
 
+    [SerializeField]
+    [Tooltip("When enabled, this object makes it seem like the card is glowing.")]
+    [NotNull]
+    GameObject glow;
+
     void Update()
     {
         attackText.text = "ATK: " + Card.Attack.ToString();
         healthText.text = "HP: " + Card.CurrentHealth.ToString();
         summonCostText.text = "COST: " + Card.SummonCost.ToString();
         nameText.text = Card.Name;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        glow.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        glow.SetActive(false);
     }
 }

2afaa8a4a16c2beadaae5a3de5570d56c10ca2c9
Jason Tu <jasont@remarkablefoods.com>
2022-08-18 08:58:26 -0400

    Add todos

--- Assets/Scripts/ScriptableObjects/TurnKeeper.cs
+++ Assets/Scripts/ScriptableObjects/TurnKeeper.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 [CreateAssetMenu]
 public class TurnKeeper : ScriptableObject
 {
+	/// <summary>
+    /// Initial turn phase upon start of game.
+    /// </summary>
+	TurnPhase turnPhase = TurnPhase.PlayerDrawsCards;
+
 	public TurnPhase CurrentTurnPhase
 	{
-		get;
-		set;
+		get { return turnPhase; }
+		set { turnPhase = value; }
     }
 
 #if false
@@ -15,11 +20,19 @@ public class TurnKeeper : ScriptableObject
 [x] play cards phase
 [x] consider turn order, or leave for another GitHub isse - don't need for MVP
 [x] opponent performs the same phases
+[x] combat phase: play now proceeds into the combat phase
 
-[ ] TODO: can end turn by clicking a bell in the center-left of the table
-[ ] combat phase: play now proceeds into the combat phase
-	[ ] monsters on the field can now attack the opponent directly (since the opponent can't yet place monsters)
-	[ ] attack points will create tokens that accrue on the opponent's side of the scale
+    [ ] PlayerDrawsCards,
+    [ ] PlayerPlaysCards,
+		[ ] TODO: can end turn by clicking a bell in the center-left of the table
+    [ ] PlayerCombatPhase,
+		[ ] monsters on the field can now attack the opponent directly (since the opponent can't yet place monsters)
+		[ ] attack points will create tokens that accrue on the opponent's side of the scale
+    [ ] OpponentDrawsCards,
+    [ ] OpponentPlaysCards,
+    [ ] OpponentCombatPhase,
+    [ ] * Game loop debugging, phases should be visualized through text, perhaps in the horizontal and vertical center of the screen
+    [ ] * game loop repetition: turns should be able to loop around. that is, after a turn ends, the next turn begins, up until one of the players wins or loses
 
 #endif
 }

1a167d53d3b6ef9de72805bfd4d056b57e25ad22
Jason Tu <jasontu4@gmail.com>
2023-02-25 08:02:00 -0500

    Small tweak

--- Assets/Scripts/TrackTarget.cs
+++ Assets/Scripts/TrackTarget.cs
@@ -9,7 +9,7 @@ public class TrackTarget : MonoBehaviour
     [SerializeField]
     private Vector3 offset = new Vector3(0f, 1.4f, 0f);
 
-    void Update()
+    private void Update()
     {
         transform.position = trackedObject.transform.position + offset;
     }

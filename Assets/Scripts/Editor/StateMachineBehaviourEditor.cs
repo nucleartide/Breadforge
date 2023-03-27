0f8232fa5c2533a7baf3fdeca707d31b8eebaba8
Jason Tu <jasontu4@gmail.com>
2023-03-27 18:38:17 -0400

    Add custom inspector for state machines

---
+++ Assets/Scripts/Editor/StateMachineBehaviourEditor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(StateMachineBehaviour), true)]
+public class StateMachineBehaviourEditor : Editor
+{
+    // https://learn.unity.com/tutorial/editor-scripting#5c7f8528edbc2a002053b5f8
+    public override void OnInspectorGUI()
+    {
+        var currentState = (target as StateMachineBehaviour).CurrentState;
+        GUILayout.Label($"Current State: {currentState}");
+        DrawDefaultInspector();
+    }
+}

963c8d9b343179c60a7a77738604ae8cde8d966b
Jason Tu <jasontu4@gmail.com>
2023-03-29 11:13:21 -0400

    Comments

--- Assets/Scripts/_Utils/StateBehaviour.cs
+++ Assets/Scripts/_Utils/StateBehaviour.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// StateBehaviour is used by StateMachineBehaviour to identify MonoBehaviours that are also states.
+/// </summary>
 public abstract class StateBehaviour : MonoBehaviour
 {
 }

7c419628d0ea8ae517913353254527b895b9e12f
Jason Tu <jasontu4@gmail.com>
2023-04-04 11:13:49 -0400

    Get rid of unused event

--- Assets/Scripts/Behaviours/PlayerAnimationEvents.cs
+++ Assets/Scripts/Behaviours/PlayerAnimationEvents.cs
@@ -7,11 +7,7 @@ public class PlayerAnimationEvents : MonoBehaviour
 
     public event EventHandler OnWalkFootstep;
 
-    public event EventHandler OnRunFootstep;
-
     private void PickaxeHit() => OnPickaxeHit?.Invoke(this, EventArgs.Empty);
 
     private void WalkFootstep() => OnWalkFootstep?.Invoke(this, EventArgs.Empty);
-
-    private void RunFootstep() => OnRunFootstep?.Invoke(this, EventArgs.Empty);
 }

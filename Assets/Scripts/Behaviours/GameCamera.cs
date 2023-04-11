d7fa92d70bc45929d2b9ca1844b1b612d1a5e1e8
Jason Tu <jasontu4@gmail.com>
2023-04-11 10:45:36 -0400

    Add min, max, initial ortho sizes

--- Assets/Scripts/Behaviours/GameCamera.cs
+++ Assets/Scripts/Behaviours/GameCamera.cs
@@ -16,20 +16,30 @@ public class GameCamera : MonoBehaviour
     private GameInputManager gameInput;
 
     [SerializeField]
-    private float cameraZoomScaleFactor = 5f;
+    private float zoomScaleFactor = 5f;
+
+    [SerializeField]
+    private float minOrthoSize = 1f;
+
+    [SerializeField]
+    private float maxOrthoSize = 10f;
+
+    [SerializeField]
+    private float initialOrthoSize = 3f;
 
     private void Awake()
     {
         cinemachineCamera.Follow = followedObject;
+        cinemachineCamera.m_Lens.OrthographicSize = initialOrthoSize;
     }
 
     private void Update()
     {
         var zoomDelta = gameInput.GetZoom();
         if (zoomDelta != 0f)
-            cinemachineCamera.m_Lens.OrthographicSize += zoomDelta * Time.deltaTime * -cameraZoomScaleFactor;
+            cinemachineCamera.m_Lens.OrthographicSize += zoomDelta * Time.deltaTime * -zoomScaleFactor;
 
         // Prevent ortho size from becoming too small.
-        cinemachineCamera.m_Lens.OrthographicSize = Mathf.Max(cinemachineCamera.m_Lens.OrthographicSize, 1f);
+        cinemachineCamera.m_Lens.OrthographicSize = Mathf.Clamp(cinemachineCamera.m_Lens.OrthographicSize, minOrthoSize, maxOrthoSize);
     }
 }

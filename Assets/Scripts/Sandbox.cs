d4284a796ddfcd68c85454f703c3c10d2bdc8a02
Jason Tu <jasontu4@gmail.com>
2023-05-05 10:52:01 -0400

    Add hot reload

--- Assets/Scripts/Sandbox.cs
+++ Assets/Scripts/Sandbox.cs
@@ -4,5 +4,11 @@ public class Sandbox : MonoBehaviour
 {
     private void Start()
     {
+        Debug.Log("sandbox start");
+        Debug.Log("sandbox start");
+    }
+
+    private void Update()
+    {
     }
 }
